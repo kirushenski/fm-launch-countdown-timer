2861a553532592ab3d869288e218b7dd98b0a85b
p1t1ch <belobeev.kirill@gmail.com>
2021-01-31 10:54:53 +0300

    setup theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import 'focus-visible'
+import '@fontsource/red-hat-text/latin-700.css'
 import '@/styles/global.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        blue: '#8385A9',
+        red: '#FB5E84',
+      },
+      neutral: {
+        white: '#fff',
+        blue: '#343650',
+        'blue-dark': '#1D1E28',
+        'blue-darkest': '#191A23',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Red Hat Text', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 }
